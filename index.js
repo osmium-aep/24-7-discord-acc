e2d1e16ec3954b24dca9c54e460bdb6de1977168
osmium.aep <145663627+osmium-aep@users.noreply.github.com>
2024-03-05 22:26:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,205 +4,205 @@ require('dotenv').config();
 
 
 const client = new Discord.Client({
-  checkUpdate: false,
+    checkUpdate: false,
 });
 
 let executedOnStart = false;
 
 let scedule = [
-  {
-    startHour: 0,
-    startMinute: 30,
-    startSecond: 1,
-    execute: () => {
-      client.user.setPresence({
-        activities: [
-          {
-            name: "Getting Ready For School",
-            type: "PLAYING",
-          },
-        ],
-        status: "idle",
-      });
-    },
-  },
-  {
-    startHour: 1,
-    startMinute: 30,
-    startSecond: 1,
-    execute: () => {
-      client.user.setPresence({
-        activities: [
-          {
-            name: "In School rn",
-            type: "PLAYING",
-          },
-        ],
-        status: "dnd",
-      });
-    },
-  },
-  {
-    startHour: 4,
-    startMinute: 0,
-    startSecond: 1,
-    execute: () => {
-      client.user.setPresence({
-        activities: [
-          {
-            name: "Learning 'c'",
-            type: "PLAYING",
-          },
-        ],
-        status: "idle",
-      });
-    },
-  },
-  {
-    startHour: 5,
-    startMinute: 0,
-    startSecond: 1,
-    execute: () => {
-      client.user.setPresence({
-        activities: [
-          {
-            name: "Cleaning Setup",
-            type: "PLAYING",
-          },
-        ],
-        status: "idle",
-      });
-    },
-  },
-  {
-    startHour: 5,
-    startMinute: 30,
-    startSecond: 1,
-    execute: () => {
-      client.user.setPresence({
-        activities: [
-          {
-            name: "Chillin ☕",
-            type: "PLAYING",
-          },
-        ],
-        status: "idle",
-      });
-    },
-  },
     {
-    startHour: 11,
-    startMinute: 20,
-    startSecond: 1,
-    execute: () => {
-      client.user.setPresence({
-        activities: [
-          {
-            name: "Getting Ready For Tution",
-            type: "PLAYING",
-          },
-        ],
-        status: "idle",
-      });
+        startHour: 0,
+        startMinute: 30,
+        startSecond: 1,
+        execute: () => {
+            client.user.setPresence({
+                activities: [
+                    {
+                        name: "Getting Ready For School",
+                        type: "PLAYING",
+                    },
+                ],
+                status: "idle",
+            });
+        },
     },
-  },
-  {
-    startHour: 11,
-    startMinute: 30,
-    startSecond: 1,
-    execute: () => {
-      client.user.setPresence({
-        activities: [
-          {
-            name: "Math's Tution",
-            type: "PLAYING",
-          },
-        ],
-        status: "dnd",
-      });
+    {
+        startHour: 1,
+        startMinute: 30,
+        startSecond: 1,
+        execute: () => {
+            client.user.setPresence({
+                activities: [
+                    {
+                        name: "In School rn",
+                        type: "PLAYING",
+                    },
+                ],
+                status: "dnd",
+            });
+        },
     },
-  },
-  {
-    startHour: 12,
-    startMinute: 30,
-    startSecond: 1,
-    execute: () => {
-      client.user.setPresence({
-        activities: [
-          {
-            name: "Free Fire",
-            type: "PLAYING",
-          },
-        ],
-        status: "idle",
-      });
+    {
+        startHour: 4,
+        startMinute: 0,
+        startSecond: 1,
+        execute: () => {
+            client.user.setPresence({
+                activities: [
+                    {
+                        name: "Learning 'c'",
+                        type: "PLAYING",
+                    },
+                ],
+                status: "idle",
+            });
+        },
     },
-  },
-  {
-    startHour: 14,
-    startMinute: 0,
-    startSecond: 1,
-    execute: () => {
-      client.user.setPresence({
-        activities: [
-          {
-            name: "Chillin ☕",
-            type: "PLAYING",
-          },
-        ],
-        status: "idle",
-      });
+    {
+        startHour: 5,
+        startMinute: 0,
+        startSecond: 1,
+        execute: () => {
+            client.user.setPresence({
+                activities: [
+                    {
+                        name: "Cleaning Setup",
+                        type: "PLAYING",
+                    },
+                ],
+                status: "idle",
+            });
+        },
     },
-  },
-  {
-    startHour: 16,
-    startMinute: 30,
-    startSecond: 1,
-    execute: () => {
-      client.user.setPresence({
-        activities: [
-          {
-            name: "Sleep",
-            type: "PLAYING",
-          },
-        ],
-        status: "dnd",
-      });
+    {
+        startHour: 5,
+        startMinute: 30,
+        startSecond: 1,
+        execute: () => {
+            client.user.setPresence({
+                activities: [
+                    {
+                        name: "Chillin ☕",
+                        type: "PLAYING",
+                    },
+                ],
+                status: "idle",
+            });
+        },
     },
-  },
+    {
+        startHour: 11,
+        startMinute: 20,
+        startSecond: 1,
+        execute: () => {
+            client.user.setPresence({
+                activities: [
+                    {
+                        name: "Getting Ready For Tution",
+                        type: "PLAYING",
+                    },
+                ],
+                status: "idle",
+            });
+        },
+    },
+    {
+        startHour: 11,
+        startMinute: 30,
+        startSecond: 1,
+        execute: () => {
+            client.user.setPresence({
+                activities: [
+                    {
+                        name: "Math's Tution",
+                        type: "PLAYING",
+                    },
+                ],
+                status: "dnd",
+            });
+        },
+    },
+    {
+        startHour: 12,
+        startMinute: 30,
+        startSecond: 1,
+        execute: () => {
+            client.user.setPresence({
+                activities: [
+                    {
+                        name: "Free Fire",
+                        type: "PLAYING",
+                    },
+                ],
+                status: "idle",
+            });
+        },
+    },
+    {
+        startHour: 14,
+        startMinute: 0,
+        startSecond: 1,
+        execute: () => {
+            client.user.setPresence({
+                activities: [
+                    {
+                        name: "Chillin ☕",
+                        type: "PLAYING",
+                    },
+                ],
+                status: "idle",
+            });
+        },
+    },
+    {
+        startHour: 16,
+        startMinute: 30,
+        startSecond: 1,
+        execute: () => {
+            client.user.setPresence({
+                activities: [
+                    {
+                        name: "Sleep",
+                        type: "PLAYING",
+                    },
+                ],
+                status: "dnd",
+            });
+        },
+    },
 ];
 
 client.on("ready", () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`Logged in as ${client.user.tag}!`);
 
-  // Execute code on start
-  if (!executedOnStart) {
-    // executeScheduledCode();
-    executeOnStart();
-    executedOnStart = true;
-  }
+    // Execute code on start
+    if (!executedOnStart) {
+        // executeScheduledCode();
+        executeOnStart();
+        executedOnStart = true;
+    }
 });
 
 function getTime() {
-  const currentDate = new Date();
-  const currentHour = currentDate.getHours();
-  const currentMinutes = currentDate.getMinutes();
-  const currentSeconds = currentDate.getSeconds();
-  return {
-    currentHour,
-    currentMinutes,
-    currentSeconds,
-  };
+    const currentDate = new Date();
+    const currentHour = currentDate.getHours();
+    const currentMinutes = currentDate.getMinutes();
+    const currentSeconds = currentDate.getSeconds();
+    return {
+        currentHour,
+        currentMinutes,
+        currentSeconds,
+    };
 }
-function executeOnStart(){
+function executeOnStart() {
     const currentTime = getTime();
     console.log(currentTime)
-    for(let i = 0; i < scedule.length; i++){
-        if(currentTime.currentHour > scedule[i].startHour && currentTime.currentHour < scedule[i + 1].startHour){
+    for (let i = 0; i < scedule.length; i++) {
+        if (currentTime.currentHour > scedule[i].startHour && currentTime.currentHour < scedule[i + 1].startHour) {
             scedule[i].execute();
-            
-        }else if(currentTime.currentHour == scedule[i].startHour && currentTime.currentHour == scedule[i + 1].startHour){
-            if(currentTime.currentMinutes > scedule[i].startMinute && currentTime.currentMinutes < scedule[i + 1].startMinute){
+
+        } else if (currentTime.currentHour == scedule[i].startHour && currentTime.currentHour == scedule[i + 1].startHour) {
+            if (currentTime.currentMinutes > scedule[i].startMinute && currentTime.currentMinutes < scedule[i + 1].startMinute) {
                 scedule[i].execute();
             }
         }
@@ -210,32 +210,32 @@ function executeOnStart(){
 
 }
 function executeScheduledCode() {
-  try {
-    const currentTime = getTime();
-    for (let i = 0; i < scedule.length; i++) {
-      const scheduleItem = scedule[i];
+    try {
+        const currentTime = getTime();
+        for (let i = 0; i < scedule.length; i++) {
+            const scheduleItem = scedule[i];
 
-      if (
-        scheduleItem.startHour === currentTime.currentHour &&
-        scheduleItem.startMinute === currentTime.currentMinutes &&
-        scheduleItem.startSecond === currentTime.currentSeconds
-      ) {
-        scheduleItem.execute();
-        console.log("Scheduled code executed at", currentTime);
-      }
+            if (
+                scheduleItem.startHour === currentTime.currentHour &&
+                scheduleItem.startMinute === currentTime.currentMinutes &&
+                scheduleItem.startSecond === currentTime.currentSeconds
+            ) {
+                scheduleItem.execute();
+                console.log("Scheduled code executed at", currentTime);
+            }
+        }
+    } catch (error) {
+        console.error("Error during scheduled code execution:", error);
     }
-  } catch (error) {
-    console.error("Error during scheduled code execution:", error);
-  }
 }
 
 setInterval(executeScheduledCode, 1000);
 
 // Error handling for client login
 try {
-  client.login(process.env["TOKEN"]);
+    client.login(process.env["TOKEN"]);
 } catch (error) {
-  console.error("Error during login:", error);
+    console.error("Error during login:", error);
 }
 
 keepAlive();
